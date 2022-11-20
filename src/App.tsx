307ff486c4674d15511c5465c7e42239627547ae
c-dance <zh.cecon@gmail.com>
2022-11-20 13:44:05 +0900

    CCQO-24 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
-import {useEffect, useState} from "react";
 
 function App() {
     const [message, setMessage] = useState([]);
@@ -18,18 +17,9 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    baemin project
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
                 <ul>
                     {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
                 </ul>
